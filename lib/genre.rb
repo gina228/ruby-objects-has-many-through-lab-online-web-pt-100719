d01d236595498b8c873b5af7f9d02426964dd717
Gina Graci <ginagraci92@gmail.com>
2019-11-09 02:36:19 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,16 @@
+class Genre 
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+    
+  def songs 
+  end
+  
+  def artists
+  end 
+  
+end 
